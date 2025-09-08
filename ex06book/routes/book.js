73da4e6654762dc23d86d9d773866fd242507c4d
jisu9169 <wltn9169@naver.com>
2025-09-08 10:29:46 +0900

    get Read books

--- ex06book/routes/book.js
+++ ex06book/routes/book.js
@@ -6,6 +6,29 @@ const router = express.Router();
 let books = [
   { id: 1, title: "Node.js 책", author: "강예진" },
   { id: 2, title: "딥러닝 책", author: "김민수" },
- ];
+];
 
+// REST API 설계
+// 1. 전체 데이터 조회
+// ([GET] localhost:3000/books)
+
+// 2. 1개 데이터 조회
+// ([GET] localhost:3000/books/{id})
+// ([GET] localhost:3000/books?id = {id})
+
+// 3. 데이터 추가
+// ([POST] localhost:3000/books)
+
+// 4. 데아터 수정(전체)
+// ([PUT] localhost:3000/books/{id})
+
+// 5. 1개 데이터 삭제
+// ([DELETE] localhost:3000/books/{id})
+
+// index 페이지 (책 정보 (books)를 가지고 이동)
+router.get("/books", (req, res) => {
+  // index.ejs 렌더링 + books 데이터
+  //                    {books(키-변수이름 : books(책 배열))}
+  res.render("index", { books: books });
+});
 module.exports = router;
