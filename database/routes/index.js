a3f4929303f8633c1f496032b34944afd76474b1
jisu9169 <wltn9169@naver.com>
2025-09-16 10:47:40 +0900

    db 연동

--- database/routes/index.js
+++ database/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const conn = require("../config/db");
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -11,6 +12,25 @@ router.get("/join", (req, res) => {
 
 router.post("/join", (req, res) => {
   console.log(req.body);
+  const { id, pw, nick } = req.body;
+
+  // 쿼리문 작성
+  const sql = "insert into member values(?,?,?)";
+
+  conn.query(sql, [id, pw, nick], (err, rows) => {
+    console.log(err ? err : rows);
+  });
+
+  res.redirect("/");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res) => {
+  console.log(req.body);
+
   res.send("ok");
 });
 module.exports = router;
